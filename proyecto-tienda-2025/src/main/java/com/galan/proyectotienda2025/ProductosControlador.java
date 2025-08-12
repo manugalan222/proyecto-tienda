7df6f927f71d450b1eb8836fd690ad1875a4f9d6
Laureano Galan <lauregalan1@gmail.com>
2025-08-12 20:29:26 -0300

    Trate de agregar los datos a la tabla de productos (en proceso)

--- proyecto-tienda-2025/src/main/java/com/galan/proyectotienda2025/ProductosControlador.java
+++ proyecto-tienda-2025/src/main/java/com/galan/proyectotienda2025/ProductosControlador.java
@@ -1,5 +1,7 @@
 package com.galan.proyectotienda2025;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -8,6 +10,7 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -23,11 +26,20 @@ public class ProductosControlador implements Initializable {
     @FXML TextField txtPrecioCompra = null;
     @FXML TextField txtPrecioVenta = null;
     @FXML private ComboBox<String> comboTemporada;
-    @FXML private ComboBox<String> comboTipo;
     @FXML private ComboBox<String> comboMarca;
     @FXML private ComboBox<Boolean> comboPromo;
     @FXML private Spinner<Integer> spinnerStock;
 
+    @FXML private TableView<Database.Producto> tablaProductos;
+    @FXML private TableColumn<Database.Producto, String> colId;
+    @FXML private TableColumn<Database.Producto, String> colNombre;
+    @FXML private TableColumn<Database.Producto, String> colDesc;
+    @FXML private TableColumn<Database.Producto, Double> colCompra;
+    @FXML private TableColumn<Database.Producto, Double> colVenta;
+    @FXML private TableColumn<Database.Producto, String> colMarca;
+    @FXML private TableColumn<Database.Producto, String> colTemporada;
+    @FXML private TableColumn<Database.Producto, Integer> colStock;
+    @FXML private TableColumn<Database.Producto, Boolean> colPromo;
 
     public void onInicioButtonClick(ActionEvent actionEvent) throws IOException {
         cambiarEscena(actionEvent, "pantalla_principal.fxml");
@@ -49,7 +61,6 @@ public class ProductosControlador implements Initializable {
 
         /* Recibe los valores para la base de datos*/
         String temporadaValor = comboTemporada.getValue();
-        String tipoValor = comboTipo.getValue();
         String marcaValor = comboMarca.getValue();
         Boolean promoValor = comboPromo.getValue();
         int stockValor = spinnerStock.getValue();
@@ -59,16 +70,32 @@ public class ProductosControlador implements Initializable {
         String productoNombre = txtProductoNombre.getText();
         String productoDesc = txtProductoDesc.getText();
 
-        productosModelo.agregarProductoBd(temporadaValor,tipoValor,marcaValor,stockValor,precioCompra,precioVenta,productoId,productoNombre,productoDesc,promoValor);
+        productosModelo.agregarProductoBd(temporadaValor,marcaValor,stockValor,precioCompra,precioVenta,productoId,productoNombre,productoDesc,promoValor);
     }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        // Configurar cómo cada columna obtiene su valor
+        colId.setCellValueFactory(new PropertyValueFactory<>("id"));
+        colNombre.setCellValueFactory(new PropertyValueFactory<>("nombre"));
+        colCompra.setCellValueFactory(new PropertyValueFactory<>("precioCompra"));
+        colVenta.setCellValueFactory(new PropertyValueFactory<>("precioVenta"));
+        colTemporada.setCellValueFactory(new PropertyValueFactory<>("temporada"));
+        colPromo.setCellValueFactory(new PropertyValueFactory<>("promocionable"));
+        colMarca.setCellValueFactory(new PropertyValueFactory<>("marca"));
+        colStock.setCellValueFactory(new PropertyValueFactory<>("stock"));
+        colDesc.setCellValueFactory(new PropertyValueFactory<>("descripcion"));
+
+        // Traer los datos de la BD y mostrarlos
+        ObservableList<Database.Producto> productosObservable = FXCollections.observableArrayList(Database.listarProductos());
+
+        tablaProductos.setItems(productosObservable);
+
         // Cargar opciones para Estado
         comboTemporada.getItems().addAll("Disponible", "No disponible", "En pedido");
 
         // Cargar opciones para Tipo
-        comboTipo.getItems().addAll("Pantalon", "Remera", "Campera", "Beanies");
 
         // Cargar opciones para Marca
         comboMarca.getItems().addAll("Sin marca");
