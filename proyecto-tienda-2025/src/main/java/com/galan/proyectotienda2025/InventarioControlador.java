3c725d44a22ea76ab907db47828539de1c59adad
Laureano Galan <lauregalan1@gmail.com>
2025-08-16 15:14:59 -0300

    Modifique los metadatos de la tabla productos

--- proyecto-tienda-2025/src/main/java/com/galan/proyectotienda2025/InventarioControlador.java
+++ proyecto-tienda-2025/src/main/java/com/galan/proyectotienda2025/InventarioControlador.java
@@ -9,6 +9,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
